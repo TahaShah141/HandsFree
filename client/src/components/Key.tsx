a3859ff9f26769cca27c51064943438c489f3ff4
TahaShah141 <tahashah411@gmail.com>
2025-01-15 02:58:41 +0500

    add tapping animation

--- client/src/components/Key.tsx
+++ client/src/components/Key.tsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react"
 import { CanClick, KeyType } from "../types"
 
 export const Key: React.FC<KeyType & CanClick> = ({display, keyCode, pressed=false, width=1, onClick}) => {
 
+  const [tapped, setTapped] = useState(false)
+  useEffect(() => {
+    if (tapped)
+      setTimeout(() => setTapped(false), 200)
+  }, [tapped])
+
   return (
-    <div onClick={() => onClick(keyCode)} style={{aspectRatio: `${width}/1`}} className={`h-12 px-2 py-1 text-xs flex justify-center items-center bg-black rounded-md text-white ${pressed ? "border-white" : "border-white/50"} border-[0.5px]`}>
+    <div onClick={() => {setTapped(true); onClick(keyCode)}} style={{aspectRatio: `${width}/1`}} className={`h-12 px-2 py-1 text-xs flex justify-center items-center bg-black rounded-md text-white ${pressed || tapped ? "border-white" : "border-white/50"} border-[0.5px]`}>
       {display}
     </div>
   )
