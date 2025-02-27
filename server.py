9cb81d3e790105390aa4c5b0262d572d9288a5e4
TahaShah141 <tahashah411@gmail.com>
2025-02-27 15:26:38 +0500

    change server mode

--- server.py
+++ server.py
@@ -146,5 +146,5 @@ def handle_scroll():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=1301, debug=True)
-    # app.run(host='0.0.0.0', port=1301)
+    # app.run(host='0.0.0.0', port=1301, debug=True)
+    app.run(host='0.0.0.0', port=1301)
